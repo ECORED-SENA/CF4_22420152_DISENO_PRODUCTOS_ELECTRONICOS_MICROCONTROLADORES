a6542648c4bb9de22a4bdac952f359e2cb425475
Edwin Sneider Velandia Suarez <ivinernesto123@gmail.com>
2024-11-26 21:49:46 -0500

    avances

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -2,10 +2,101 @@
 .curso-main-container.pb-3
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
-    .titulo-principal.color-acento-contenido
+    .titulo-principal.color-acento-contenido.mb-0(data-aos="flip-up")
       .titulo-principal__numero
         span 3
-      h1 Titulo de primer nivel
+      h1 Reciclaje electrónico #[em (e-waste)]
+    
+    .fondo-personalizado-1.f2
+      .fondo-personalizado-1__contenido.pt-5
+        .row
+          .col-lg-8(data-aos="fade-right")
+            p.mb-0 Reciclar implica aprovechar los residuos de aparatos eléctricos y electrónicos dañados, obsoletos o en desuso, evitando así su impacto ambiental. El término "#[em e-waste]" abarca dispositivos eléctricos que incluyen computadoras, teléfonos móviles y electrodomésticos.
+          .col-lg-4.order-first.order-lg-last(data-aos="fade-left")
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/35.png",style="max-width: 400px;")
+        
+        .titulo-decorativo.my-5(data-aos="fade-up")
+          .cont-figura
+            img(src="@/assets/curso/temas/img-tit-dec.svg")
+          .cont-text 
+            h3 Materiales nocivos al medio ambiente
+        
+        .row.align-items-center
+          .col(data-aos="fade-right")
+            p.mb-0 Los dispositivos electrónicos contienen sustancias tóxicas como #[b plomo, cadmio, cromo, mercurio, selenio y arsénico], que son peligrosas para la salud y el ecosistema al ser liberadas en el ambiente.
+          .col-md-auto.order-first.order-md-last(data-aos="fade-left")
+            figure.mb-4.mb-md-0
+              img.mx-auto(src="@/assets/curso/temas/36.svg",style="max-width: 84px;")
+        
+        .titulo-decorativo.my-5(data-aos="fade-up")
+          .cont-figura
+            img(src="@/assets/curso/temas/img-tit-dec.svg")
+          .cont-text 
+            h3 ¿Qué hacer con nuestros residuos electrónicos?
+        
+        .row
+          .col-lg-8(data-aos="fade-right")
+            p.mb-0 El manejo de estos residuos, según la Directiva Europea 2003/108/CE, es responsabilidad de fabricantes y entidades locales, estableciendo también medidas de prevención para evitar sustancias peligrosas en el diseño y fabricación de nuevos aparatos.
+          .col-lg-4.order-first.order-lg-last(data-aos="fade-left")
+            figure.mb-4.mb-lg-0
+              img.mx-auto(src="@/assets/curso/temas/37.png",style="max-width: 400px;")
+    
+    .titulo-decorativo.my-5(data-aos="fade-up")
+      .cont-figura
+        img(src="@/assets/curso/temas/img-tit-dec.svg")
+      .cont-text 
+        h3 Programas de recogida
+    
+    .row.align-items-center.mb-5
+      .col(data-aos="fade-right")
+        p.mb-0 Existen programas en los que los usuarios pagan una tasa de reciclaje al comprar el producto, mientras que en otros es la industria quien asume los costos. Algunas recomendaciones incluyen:
+      .col-md-auto.order-first.order-md-last(data-aos="fade-left")
+        figure.mb-4.mb-md-0
+          img.mx-auto(src="@/assets/curso/temas/38.svg",style="max-width: 84px;")
+    
+    .fondo-personalizado-1.size-full.med.pos-bottom.f3
+      .fondo-personalizado-1__contenido.pb-5
+        .row.justify-content-center.pb-4(data-aos="zoom-in-out")
+          .col-lg-8
+            .fondo-color-sistema-n.borde-21.p-3.sombra-p-2.mb-3
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/39.svg",style="max-width: 97px;")
+                .col
+                  p.mb-0 No desechar dispositivos electrónicos con basura común.
+            .fondo-color-sistema-n.borde-21.p-3.sombra-p-2.mb-3
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/40.svg",style="max-width: 97px;")
+                .col
+                  p.mb-0 Donar dispositivos a personas u organizaciones que los necesiten.
+            .fondo-color-sistema-n.borde-21.p-3.sombra-p-2.mb-3
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/41.svg",style="max-width: 97px;")
+                .col
+                  p.mb-0 Reparar o reutilizar piezas de los dispositivos.
+            .fondo-color-sistema-n.borde-21.p-3.sombra-p-2.mb-3
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/42.svg",style="max-width: 97px;")
+                .col
+                  p.mb-0 Invertir en plantas de reciclaje de desechos electrónicos.
+            .fondo-color-sistema-n.borde-21.p-3.sombra-p-2.mb-3
+              .row.align-items-center
+                .col-md-auto
+                  figure.mb-4.mb-lg-0
+                    img.mx-auto(src="@/assets/curso/temas/43.svg",style="max-width: 97px;")
+                .col
+                  p.mb-0 Reciclar productos no reutilizables como lámparas de bajo consumo.
+    
+
+
 
 </template>
 
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,12 +2,13 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
+    p.mb-5 A continuación, se presenta una síntesis de la temática estudiada en el componente formativo.
 
     .row.justify-content-center
       .col-lg-10.mb-5
-        figure
-          img(src="@/assets/curso/temas/sintesis.svg", alt="alt")
+        .fondo-color-sistema-h.p-4.py-5.borde-20
+          figure
+            img(src="@/assets/curso/temas/sintesis.svg", alt="Síntesis  sobre la optimización de procesos y reciclaje electrónico. Incluye: reingeniería para mejorar costo y calidad de productos electrónicos; reciclaje de dispositivos obsoletos para reducir el impacto ambiental; materiales tóxicos como plomo y cadmio, con riesgos para la salud; y gestión de residuos con acciones de reciclaje y reparación a cargo de fabricantes y entidades locales.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
